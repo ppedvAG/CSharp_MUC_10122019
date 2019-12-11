6efe5f04877d39584b0b1616bb0ca73a4b4efa62
André <andrer@ppedv.de>
2019-12-11 10:19:44 +0100

    Hallo Klassen mit Achterbahnen

---
+++ HalloKLassen/HalloKLassen/Achterbahn.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloKlassen
+{
+    class Achterbahn
+    {
+        private string name; //datenfeld
+
+        #region getter setter methode (macht so keiner!)
+        //getter funktion
+        public string GetName()
+        {
+            return name;
+        }
+
+        //setter methode
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        #endregion
+
+        //full property
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //auto property
+        public string Hersteller { get; set; }
+        public string Modell { get; set; }
+        public int Höchstgeschwindigkeit { get; set; }
+        public double Länge { get; set; }
+        public double Höhe { get; set; }
+        public int Sitzplätze { get; set; }
+        public DateTime Baujahr { get; set; }
+    }
+}
---
+++ HalloKLassen/HalloKLassen/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Hallo Klassen ***");
+
+            Achterbahn meineAchterbahn; //deklaration
+            meineAchterbahn = new Achterbahn(); //instanzierung
+            meineAchterbahn.Name = "Silverstar";
+            meineAchterbahn.Hersteller = "MackRide";
+            meineAchterbahn.Modell = "LSM Launch coster";
+            meineAchterbahn.Höchstgeschwindigkeit = 121;
+            meineAchterbahn.Höhe = 70;
+            meineAchterbahn.Länge = 1.2;
+            meineAchterbahn.Baujahr = new DateTime(2002, 3, 23);
+            meineAchterbahn.Sitzplätze = 36;
+
+            Achterbahn nochEineAchterbahn = new Achterbahn();
+            nochEineAchterbahn.Name = "Olympia Looping";
+            nochEineAchterbahn.Hersteller = "Schwarzkopf";
+            nochEineAchterbahn.Modell = "Klassik Lift";
+            nochEineAchterbahn.Höchstgeschwindigkeit = 90;
+            nochEineAchterbahn.Höhe = 47;
+            nochEineAchterbahn.Länge = 0.7;
+            nochEineAchterbahn.Baujahr = new DateTime(1989, 1, 1);
+            nochEineAchterbahn.Sitzplätze = 28;
+
+            ZeigeAchterbahn(meineAchterbahn);
+            ZeigeAchterbahn(nochEineAchterbahn);
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+
+        static void ZeigeAchterbahn(Achterbahn bahn)
+        {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Name: {bahn.Name} Hersteller: {bahn.Hersteller} Modell: {bahn.Modell}");
+            Console.WriteLine($"MaxSpeed: {bahn.Höchstgeschwindigkeit} kmn/h");
+            Console.WriteLine($"Höhe: {bahn.Höhe:0.00}m Länge: {bahn.Länge}m");
+            //Console.WriteLine("Höhe: {0}m Länge: {1}m", bahn.Höhe, bahn.Länge); //OLD STyle
+            Console.WriteLine($"Sitzplätze: {bahn.Sitzplätze}");
+            Console.WriteLine($"Baujahr: {bahn.Baujahr:d}");
+        }
+    }
+}
