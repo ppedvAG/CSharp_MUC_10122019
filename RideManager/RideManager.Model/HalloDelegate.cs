5331cad2c45e779dbc216cf1fb21dfab76fbedf8
André <andrer@ppedv.de>
2019-12-13 10:47:14 +0100

    Delegates Simple

---
+++ RideManager/RideManager.Model/HalloDelegate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideManager.Model
+{
+
+    delegate void EinfacheDelegate();
+    delegate void DelegateMitParameter(string text);
+    delegate long CalcDelegate(int a, int b);
+
+    class HalloDelegate
+    {
+        public HalloDelegate()
+        {
+            EinfacheDelegate meineDelegate = EinfacheMethode;
+            DelegateMitParameter meinDeleMitPara = MethodeMitPara;
+            CalcDelegate calc = Minus;
+            long result = calc.Invoke(3, 4);
+        }
+
+        private long Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        private long Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public void MethodeMitPara(string bla)
+        {
+            Console.WriteLine("Hallo" + bla);
+        }
+        public void EinfacheMethode()
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+}
